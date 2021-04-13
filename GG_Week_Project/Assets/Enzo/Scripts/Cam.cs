9f7ba80ff07551a42bf5371372df7c4809027de6
Bifflou <gut.enzo02122001@gmail.com>
2021-04-13 11:18:35 +0200

    Correction menu et import cinemachine
    on prie pour que ça ne casse pas tout

---
+++ GG_Week_Project/Assets/Enzo/Scripts/Cam.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject Player2;
+    void Update()
+    {
+
+    }
+}
